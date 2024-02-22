afecf6abf20fe5fd3de6d624129471beee331b76
Boti05 <erdelyi.botond1@gmail.com>
2024-02-22 19:59:08 +0100

    making the before_start event

--- memory_game/memory_game.ino
+++ memory_game/memory_game.ino
@@ -20,19 +20,64 @@ int button5 = 14;
 int button6 = 7;
 int button7 = 5;
 int button8 = 3;
-int button_Array[] = {button1, button2, button3, button4, button5, button6, button7, button8, button9};
+int button9 = 2;
+int Button_Array[] = {button1, button2, button3, button4, button5, button6, button7, button8, button9};
 //set the useful variables
 bool pressed = false;
 bool not_pressed = true;
 
 //make function for lighting with buttons
 void light_button(int button, int led) {
-  while (digitalRead(button) == false) {
+  while (digitalRead(button) == pressed) {
     digitalWrite(led, HIGH);
   }
-  while (digitalRead(button) == true) {
+  while (digitalRead(button) == not_pressed) {
     digitalWrite(led, LOW);
   }
+}
+
+//making a function for knowing which button was pressed and with it starting the game if it was't
+int startin_process() {
+  int start = 0;
+  for (byte i = 0; i < 11; i++) {
+    if (digitalRead(Button_Array[i]) == pressed) {
+      return start = 1;
+    }
+  }
+}
+
+//making the "led game" before starting the actual game
+void before_start(int start) {
+  int led_num = 0;
+  while (start == 0) {
+    for (byte i = 0; i < 11; i++) {
+      digitalWrite(Led_Array[i], HIGH);
+      delay(50);
+      digitalWrite(Led_Array[i+1], HIGH);
+      digitalWrite(Led_Array[i], LOW);
+      delay(50);
+    }
+    for (byte j = 11; j > 2 ; j--) {
+      digitalWrite(Led_Array[j], LOW);
+      digitalWrite(Led_Array[j-2], HIGH);
+      delay(50);
+      digitalWrite(Led_Array[j-2], LOW);
+      digitalWrite(Led_Array[j-3], HIGH);
+      delay(50);
+    }
+  }
+  // do while ha az érték 1, a végén 2 lesz hogy csak egyszer fusson le és a másik ne fusson
+    digitalWrite(Led_Array[-1], LOW);
+    digitalWrite(Led_Array[-2], LOW);
+    for (byte k = 0; k = 3; k++) {
+      for (byte l = 0; l < 9; l++) {
+        digitalWrite(Led_Array[l], LOW);
+      }
+      delay(100);
+      for (byte b = 0; b < 9; b++) {
+        digitalWrite(Led_Array[b], HIGH);
+      }
+    }
 }
 
 void tick() {
@@ -52,13 +97,14 @@ void setup() {
   for (byte i = 0; i < 12; i ++) {
     pinMode(Led_Array[i], OUTPUT);
   }
-
   //make the arduino be abel to print to the serial monitor
   Serial.begin(9600);
 }
 
 void loop() {
-  tick();
+
+
+  before_start(startin_process());
 
   for (byte i = 0; i < 11; i++) {
     Serial.println(Led_Array[i]);
